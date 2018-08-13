5c0d667f552e7dff35383df62f4dfc1bf96bc010
Chery Vang <misscheryvang@yahoo.com>
2018-08-12 23:25:15 -0500

    initial project done. Isn't working completely

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Avocado Chicken burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Triple cheese burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Spicy Salmon burger", false);
