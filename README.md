78ac165e230f53abf7070335f05adc32c65d84b7
Chery Vang <misscheryvang@yahoo.com>
2018-08-13 11:07:50 -0500

    got burgers displayed.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # burger
+
+This app is a burger logger and it allows users to enter a burger and devoured any burgers avaliable.
+
+This app is dependent on node packages from MySQL, express and body-parser.
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF exists burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
@@ -5,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY(id)
 );
